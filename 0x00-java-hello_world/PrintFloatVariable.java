a196b88efd7a808935e2dd2dedd170fc8093464a
Mbongeni Mahlangu <mbongenimahlangu203@outlook.com>
2026-04-07 10:20:08 +0000

    fix: rectified value being formatted, after evaluation

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,6 +1,6 @@
 public class PrintFloatVariable{
   public static void main(String[] args){
     float pi = (float) 3.141592; 
-    System.out.println("Pi Value: " + String.format("%.3f",value));
+    System.out.println("Pi Value: " + String.format("%.3f",pi));
   }
 }
