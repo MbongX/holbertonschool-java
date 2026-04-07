d22480bfd1227555dcf4f81f48b271608fbd0bbe
Mbongeni Mahlangu <mbongenimahlangu203@outlook.com>
2026-04-07 10:17:49 +0000

    Optimized submission solution to not use any imports

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,9 +1,6 @@
-import java.math.*;
-
 public class PrintFloatVariable{
   public static void main(String[] args){
     float pi = (float) 3.141592; 
-    BigDecimal value = new BigDecimal(Float.toString(pi)).setScale(3, RoundingMode.DOWN);
-    System.out.println("Pi Value: " + value);
+    System.out.println("Pi Value: " + String.format("%.3f",value));
   }
 }
