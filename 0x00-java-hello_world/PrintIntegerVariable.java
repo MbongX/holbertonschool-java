5f573a75c5bc492cb4198fb927fca2096b8b7c92
Mbongeni Mahlangu <mbongenimahlangu203@outlook.com>
2026-04-07 09:01:18 +0000

    Submission of Task 2 - Printing an Integer and addition of gitignore

---
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -0,0 +1,8 @@
+public class PrintIntegerVariable(){
+  int postalCode = 1600;
+
+  public static void main(String[] args) {
+    PrintIntegerVariable obj = new PrintIntegerVariable(); 
+    System.out.println("Pennsylvania Avenue, " + obj.postalCode);
+  }
+}
--- 0x00-java-hello_world/README.md
+++ 0x00-java-hello_world/README.md
@@ -1,1 +1,7 @@
 # Holbertonschool-java | 0x00
+
+ ** Task 1 **
+ - Printing a line in java
+
+ ** Task 2 **
+ - Printing an integer variable in java 
