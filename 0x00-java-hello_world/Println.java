272b2b17b39198782d539a18eb678024bccd2f42
Mbongeni Mahlangu <mbongenimahlangu203@outlook.com>
2026-04-07 08:49:20 +0000

    Updated java class after evaluation

--- 0x00-java-hello_world/Println.java
+++ 0x00-java-hello_world/Println.java
@@ -1,8 +1,8 @@
 public class Println{
-  String name = "Holberton School";
+  String name = "Holberton School\n";
 
   public static void main(String[] args) {
     Println obj = new Println();
-    System.out.println(obj.name + "\n");
+    System.out.println(obj.name);
   }
 }
