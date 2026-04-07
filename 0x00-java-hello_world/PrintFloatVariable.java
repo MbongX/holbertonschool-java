fabc48f7056de6e805b8c8cf29a734cbae1be7b0
Mbongeni Mahlangu <mbongenimahlangu203@outlook.com>
2026-04-07 09:58:25 +0000

    trimmmed down file

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,13 +1,9 @@
-import java.math.BigDecimal;
-import java.math.RoundingMode;
+import java.math.*;
 
 public class PrintFloatVariable{
-  float pi = (float)3.141592;
-
   public static void main(String[] args){
-    PrintFloatVariable fl = new PrintFloatVariable();
-    BigDecimal value = new BigDecimal(Float.toString(fl.pi)).setScale(3, RoundingMode.DOWN);
-  
+    float pi = (float) 3.141592; 
+    BigDecimal value = new BigDecimal(Float.toString(pi)).setScale(3, RoundingMode.DOWN);
     System.out.println("Pi Value: " + value);
   }
 }
